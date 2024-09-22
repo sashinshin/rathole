124d6cc27dee0d3a8f73599faa5493801f578615
sashaluhr <sasha.luhr@gmail.com>
2024-09-22 23:42:48 +0200

    dynamically generate image positions for desktop

--- src/App.tsx
+++ src/App.tsx
@@ -21,3 +21,13 @@ const App: React.FC = () => {
 };
 
 export default App;
+// const images = [
+//   { src: "/resources/profile_ash.jpg", alt: "Image 1", top: "5%", left: "10%", width: "20%" },
+//   { src: "/resources/profile_ash.jpg", alt: "Image 2", top: "40%", left: "25%", width: "15%" },
+//   { src: "/resources/tattoo_1.jpeg", alt: "Image 3", top: "15%", left: "40%", width: "25%" },
+//   { src: "/resources/profile_ash.jpg", alt: "Image 4", top: "50%", left: "55%", width: "20%" },
+//   { src: "/resources/tattoo_design_1.jpeg", alt: "Image 5", top: "10%", left: "70%", width: "30%" },
+//   { src: "/resources/profile_ash.jpg", alt: "Image 6", top: "60%", left: "5%", width: "35%" },
+//   { src: "/resources/profile_ash.jpg", alt: "Image 7", top: "80%", left: "50%", width: "20%" },
+//   { src: "/resources/profile_ash.jpg", alt: "Image 8", top: "70%", left: "75%", width: "25%" },
+// ];
---
+++ src/interfaces.ts
@@ -0,0 +1,10 @@
+export interface ImageStyles {
+    top: string;
+    left: string;
+    width: string;
+  }
+  
+export interface ImageData extends ImageStyles {
+    src: string;
+    alt: string;
+  }
